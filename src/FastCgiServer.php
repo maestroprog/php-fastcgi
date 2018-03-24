c24ab37a6335ed02523b1ab92565a4466f64e947
Ruslan Yarullin <maestroprog@gmail.com>
2018-03-24 23:24:04 +0300

    Temporary using swoole extension.

--- src/FastCgiServer.php
+++ src/FastCgiServer.php
@@ -3,10 +3,13 @@
 namespace Maestroprog\PhpFpm;
 
 use Esockets\Base\Configurator;
-use Esockets\Client;
 use Esockets\Socket\Ipv4Address;
 use Protocol\FCGI;
+use Protocol\FCGI\FrameParser;
 use Protocol\FCGI\Record;
+use Protocol\FCGI\Record\EndRequest;
+use Protocol\FCGI\Record\Stdout;
+use Swoole\Server;
 
 class FastCgiServer
 {
@@ -16,17 +19,18 @@ class FastCgiServer
     protected $dispatchSignals;
     protected $work;
 
-    /** @var Client[] */
+//    /** @var Client[] */
     private $clients = [];
     /** @var Record[][] */
     private $records = [];
 
     public function __construct(Configurator $configurator, RequestHandlerInterface $requestHandler)
     {
-        $this->server = $configurator->makeServer();
+        $this->server = new Server('localhost', 9002, SWOOLE_PROCESS, SWOOLE_TCP);
+//        $this->server = $configurator->makeServer();
         $this->requestHandler = $requestHandler;
 
-        $this->dispatchSignals = extension_loaded('pcntl');
+//        $this->dispatchSignals = extension_loaded('pcntl');
         if ($this->dispatchSignals) {
             pcntl_signal(defined('SIGINT') ? SIGINT : 2, [$this, 'dispatch']);
             pcntl_signal(defined('SIGTERM') ? SIGTERM : 15, [$this, 'dispatch']);
@@ -37,7 +41,8 @@ class FastCgiServer
 
     public function open(Ipv4Address $address): self
     {
-        $this->server->connect($address);
+        $this->server->start();
+//        $this->server->connect($address);
 
         return $this;
     }
@@ -46,26 +51,25 @@ class FastCgiServer
     {
         $this->work = true;
 
-        if (pcntl_fork()>0){
-            pcntl_fork();
-        }
+//        if (pcntl_fork() > 0) {
+//            pcntl_fork();
+//        }
 
-        while ($this->work) {
-            $this->server->find();
+        /*while ($this->work) {
             if ($this->dispatchSignals) {
                 pcntl_signal_dispatch();
             }
 
             $this->handleRequests();
-        }
+        }*/
 
         return $this;
     }
 
     public function stop(): self
     {
-        $this->server->disconnectAll();
-        $this->server->disconnect();
+        $this->server->stop();
+//        $this->server->disconnect();
 
         return $this;
     }
@@ -76,7 +80,7 @@ class FastCgiServer
     }
 
     protected function init(): void
-    {
+    {/*
         $this
             ->server
             ->onFound(function (Client $client): void {
@@ -94,6 +98,43 @@ class FastCgiServer
                     unset($this->records[$id]);
                     unset($this->clients[$id]);
                 });
+            });*/
+
+        $this
+            ->server
+            ->on('connect', function ($serv, $fd) {
+                $id = $fd;
+
+                $this->clients[$id] = $fd;
+            });
+
+        $this
+            ->server
+            ->on('receive', function ($serv, $fd, $from_id, $data) {
+                $buffer = '';
+                if (null === $data && empty($buffer)) {
+                    return null;
+                }
+                $buffer .= $data;
+
+                $frames = [];
+                do {
+                    $frames[] = FrameParser::parseFrame($buffer);
+                } while (strlen($buffer));
+
+                $this->records[$fd] = array_merge($this->records[$fd] ?? [], $frames);
+                $this->handleRequests();
+            });
+
+        $this
+            ->server->on('close', function ($serv, $fd) {
+
+                if (isset($this->records[$fd])){
+                    unset($this->records[$fd]);
+                }
+                if (isset($this->clients[$fd])){
+                    unset($this->clients[$fd]);
+                }
             });
     }
 
@@ -130,11 +171,39 @@ class FastCgiServer
         if (!isset($this->clients[$clientId])) {
             throw new \InvalidArgumentException(sprintf('Invalid response for client "%d", unknown client.', $clientId));
         }
+        $data = $response;
 
-        $this->clients[$clientId]->send($response);
+        if ($data instanceof FastCgiResponse) {
+            /** @var Record[] $frames */
+            static $frames = [];
+            if (empty($frames)) {
+                $frames[] = new Stdout();
+                $frames[] = new Stdout();
+                $frames[] = new EndRequest();
+            }
+            $headers = [];
+            foreach ($data->getHeaders() as $header => $value) {
+                $headers[] = "{$header}: {$value}";
+            }
+            $headers = implode("\r\n", $headers);
+            $frames[0]->setContentData($headers . "\r\n\r\n" . $data->getBody());
+
+            array_walk($frames, function (Record $record) use ($data) {
+                $record->setRequestId($data->getRequest()->getRequest()->getRequestId());
+            });
+
+            $data = implode('', $frames);
+        }
+        /*
+                $this->clients[$clientId]->send($response);
+
+                if (($response->getRequest()->getRequest()->getFlags() & FCGI::KEEP_CONN) === 0) {
+                    $this->clients[$clientId]->close();
+                }*/
+        $this->server->send($clientId, $data);
 
         if (($response->getRequest()->getRequest()->getFlags() & FCGI::KEEP_CONN) === 0) {
-            $this->clients[$clientId]->disconnect();
+            $this->server->close($clientId);
         }
     }
 }
